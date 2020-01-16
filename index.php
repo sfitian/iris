c5edff7089d878e1919417725bd4bc52f5c84b5e
Dwayne <dwayne.ferns16@gmail.com>
2020-01-16 15:19:37 +0530

    minor changes

--- index.php
+++ index.php
@@ -64,9 +64,9 @@
 	          	</div>
 		          <div class="one-forth js-fullheight d-flex align-items-start align-items-md-center ftco-animate" data-scrollax=" properties: { translateY: '70%' }">
 		          	<div class="text mt-4 mt-md-0">
-			            <h1 class="mb-4">Annual <span>Conference</span> 2019</span></h1>
-			            <h2 class="mb-4">November 26-30, 2019 - 08:00am-12:00pm</h2>
-			            <p><a href="#" class="btn btn-primary py-3 px-4">Get Ticket</a> <a href="#" class="btn btn-white py-3 px-4">Watch Video</a></p>
+			            <h1 class="mb-4">IRIS <span>Convergence</span> 2020</span></h1>
+			            <h2 class="mb-4">January 29-30</h2>
+			            <p><a href="#" class="btn btn-primary py-3 px-4">Get Ticket</a> <a href="" class="btn btn-white py-3 px-4">Watch Video</a></p>
 		            </div>
 		          </div>
 	        	</div>
@@ -107,7 +107,7 @@
     				<div class="row justify-content-start pb-3">
 		          <div class="col-md-12 heading-section ftco-animate">
 		          	<span class="subheading">Welcome</span>
-		            <h2 class="mb-4" style="font-size: 34px; text-transform: capitalize;">About Me</h2>
+		            <h2 class="mb-4" style="font-size: 34px; text-transform: capitalize;">About Iris</h2>
 		            <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
 		            <p>The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word "and" and the Little Blind Text should turn around and return to its own, safe country. But nothing the copy said could convince her and so it didn’t take long until a few insidious Copy Writers ambushed her, made her drunk with Longe and Parole and dragged her into their agency, where they abused her for their.</p>
 		          </div>
