a3dc2125a863b41d825f389466e9c10f0ee3f02e
Alan Dsilva <deathbringer2604@gmail.com>
2020-01-16 20:03:02 +0530

    Adds day 1 short summary content

--- schedule.php
+++ schedule.php
@@ -25,7 +25,7 @@
 	          <div class="col-md-8 tab-wrap">
 
 	            <div class="tab-content" id="v-pills-tabContent">
-
+<!-- --------------------- day 1 --------------------- -->
 	              <div class="tab-pane fade show active" id="v-pills-1" role="tabpanel" aria-labelledby="day-1-tab">
 	              	<div class="speaker-wrap ftco-animate d-flex">
 	              		<div class="img speaker-img" style="background-image: url(images/person_1.jpg);"></div>
@@ -51,8 +51,36 @@
 	              			<p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
 	              		</div>
 	              	</div>
+	                <div class="speaker-wrap ftco-animate d-flex">
+	              		<div class="img speaker-img" style="background-image: url(images/person_1.jpg);"></div>
+	              		<div class="text pl-md-5">
+	              			<span class="time">03:00AM - 04:30AM</span>
+	              			<h2><a href="#">Intercollege Singing Competition</a></h2>
+	              			<p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
+	              			<h3 class="speaker-name">&mdash; <a href="#">Brett Morgan</a> <span class="position">Founder of Wordpress</span></h3>
+	              		</div>
+	              	</div>
+	              	<div class="speaker-wrap ftco-animate d-flex">
+	              		<div class="img speaker-img" style="background-image: url(images/person_2.jpg);"></div>
+	              		<div class="text pl-md-5">
+	              			<span class="time">04:30AM</span>
+	              			<h2><a href="#">Evacuation</a></h2>
+	              			<p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
+	              			<h3 class="speaker-name">&mdash; <a href="#">Brett Morgan</a> <span class="position">Founder of Wordpress</span></h3>
+	              		</div>
+	              	</div>
+	              	<div class="speaker-wrap ftco-animate d-flex">
+	              		<div class="img speaker-img" style="background-image: url(images/person_3.jpg);"></div>
+	              		<div class="text pl-md-5">
+	              			<span class="time">05:00AM</span>
+	              			<h2><a href="#">Entry</a></h2>
+	              			<p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
+	              			<h3 class="speaker-name">&mdash; <a href="#">Brett Morgan</a> <span class="position">Founder of Wordpress</span></h3>
+	              		</div>
+	              	</div>
 	              </div>
 
+<!-- --------------------- day 2 --------------------- -->
 	              <div class="tab-pane fade" id="v-pills-2" role="tabpanel" aria-labelledby="v-pills-day-2-tab">
 	              	<div class="speaker-wrap ftco-animate d-flex">
 	              		<div class="img speaker-img" style="background-image: url(images/person_1.jpg);"></div>
@@ -81,7 +109,37 @@
 	              			<h3 class="speaker-name">&mdash; <a href="#">Brett Morgan</a> <span class="position">Founder of Wordpress</span></h3>
 	              		</div>
 	              	</div>
+	              	<div class="speaker-wrap ftco-animate d-flex">
+	              		<div class="img speaker-img" style="background-image: url(images/person_1.jpg);"></div>
+	              		<div class="text pl-md-5">
+	              			<span class="time">05:30AM - 06:30AM</span>
+	              			<h2><a href="#">Standup/ The Dharvi Project</a></h2>
+	              			<p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
+	              			<h3 class="speaker-name">&mdash; <a href="#">Brett Morgan</a> <span class="position">Founder of Wordpress</span></h3>
+	              		</div>
+	              	</div>
+	              	<div class="speaker-wrap ftco-animate d-flex">
+	              		<div class="img speaker-img" style="background-image: url(images/person_2.jpg);"></div>
+	              		<div class="text pl-md-5">
+	              			<span class="time">06:30AM - 09:00AM</span>
+	              			<h2><a href="#">Ragas Competition</a></h2>
+	              			<p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
+	              			<h3 class="speaker-name">&mdash; <a href="#">Brett Morgan</a> <span class="position">Founder of Wordpress</span></h3>
+	              		</div>
+	              	</div>
+	              	<div class="speaker-wrap ftco-animate d-flex">
+	              		<div class="img speaker-img" style="background-image: url(images/person_3.jpg);"></div>
+	              		<div class="text pl-md-5">
+	              			<span class="time">09:00AM - 09:45AM</span>
+	              			<h2><a href="#">DJ, Enkore, Kaam Bhaari (tentative)</a></h2>
+	              			<p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
+	              			<!-- <h3 class="speaker-name">&mdash; <a href="#">Brett Morgan</a> <span class="position">Founder of Wordpress</span></h3> -->
+	              		</div>
+	              	</div>
 	              </div>
+
+<!-- --------------------- day 3 --------------------- -->
+
 	              <div class="tab-pane fade" id="v-pills-3" role="tabpanel" aria-labelledby="v-pills-day-3-tab">
 	              	<div class="speaker-wrap ftco-animate d-flex">
 	              		<div class="img speaker-img" style="background-image: url(images/person_1.jpg);"></div>
@@ -111,6 +169,10 @@
 	              		</div>
 	              	</div>
 	              </div>
+
+
+<!-- --------------------- day 4 --------------------- -->
+
 	              <div class="tab-pane fade" id="v-pills-4" role="tabpanel" aria-labelledby="v-pills-day-4-tab">
 	              	<div class="speaker-wrap ftco-animate d-flex">
 	              		<div class="img speaker-img" style="background-image: url(images/person_1.jpg);"></div>
