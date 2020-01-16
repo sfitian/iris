6c1e9617c0a1265471e48fda66623a0bd81622a2
sank8dalvi <sanket.dalvi.ssd@gmail.com>
2020-01-16 21:18:45 +0530

    Change the title to IRIS

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>MeetUp - Free Bootstrap 4 Template by Colorlib</title>
+    <title>IRIS SFIT 2020</title>
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
