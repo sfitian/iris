2f06e22b432ce20973477865a2a018ee89e0ba1c
Ashley Dsouza <ashleydsouza639@gmail.com>
2020-01-17 15:43:34 +0530

    change redirect location

--- raagas/sub.php
+++ raagas/sub.php
@@ -18,7 +18,12 @@ $noofmembers=$_POST['noofmembers'];
  ];
  $ref="contact_form_data/";
  $pushData =$database->getReference($ref)->push($data);
+/*  $msg = "First line of text\nSecond line of text";
 
+// use wordwrap() if lines are longer than 70 characters
+$msg = wordwrap($msg,70);
+ mail("draculadsouza42@gmail.com","My subject",$msg);
+ */
  header("Location:index.php");
 } 
 
