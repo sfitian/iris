8e389287c325a8a33ca9e15f7f7928319aae4271
Jesdin Raphael <jesdinraphael@gmail.com>
2020-01-17 16:25:29 +0530

    correct path

--- header.php
+++ header.php
@@ -27,7 +27,7 @@
       <link rel="apple-touch-icon" sizes="180x180" href="/iris/images/favicon/apple-touch-icon.png">
       <link rel="icon" type="image/png" sizes="32x32" href="/iris/images/favicon/favicon-32x32.png">
       <link rel="icon" type="image/png" sizes="16x16" href="/iris/images/favicon/favicon-16x16.png">
-      <link rel="manifest" href="/site.webmanifest">
+      <link rel="manifest" href="/iris/images/favicon/site.webmanifest">
 
       <style>
           .irisLogo {
