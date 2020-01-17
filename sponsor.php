2d989083bb1a54c90bad28695eeb8e958ee9a670
ansel2000 <ansel20@gmail.com>
2020-01-17 17:05:14 +0530

    Change sponsor

--- sponsor.php
+++ sponsor.php
@@ -4,7 +4,7 @@
     ============================-->
     <section id="supporters" class="ftco-section bg-light">
 
-<div class="container">
+<!--<div class="container">
     <div class="section-header">
     <div class="row justify-content-center pb-5">
           <div class="col-md-12 heading-section text-center ftco-animate">
@@ -42,8 +42,71 @@
 
 </div>
 
-</section>
+</section> -->
+<section class="ftco-section ftco-speakers" id="speakers-section">
+			<div class="container">
+				<div class="row justify-content-center pb-5">
+          <div class="col-md-12 heading-section heading-section-white text-center ftco-animate">
+          	<span class="subheading">Sponsors</span>
+            <h2 class="mb-4">Meet Our Sponsors</h2>
+          </div>
+        </div>
+        <div class="row">
+					<div class="col-sm-6 col-md-6 col-lg-3 ftco-animate">
+						<div class="staff">
+							<div class="img-wrap d-flex  justify-content-end">
+								<div class="img" style="background-image: url(images/sponsors/VD.jpg);"></div>
+							</div>
+							<div class="text d-flex align-items-center pt-3">
+								<div>
+									<h3 class="mb-2">VD Global <br>Pvt Ltd.</h3>
+		            </div>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-6 col-md-6 col-lg-3 ftco-animate">
+						<div class="staff">
+							<div class="img-wrap d-flex align-items-stretch justify-content-end">
+								<div class="img align-self-stretch" style="background-image: url(images/sponsors/DDPL.png);"></div>
+							</div>
+							<div class="text d-flex align-items-center pt-3">
+								<div>
+									<h3 class="mb-2"> Dharmanandan<br>Diamonds</h3>
+		            </div>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-6 col-md-6 col-lg-3 ftco-animate">
+						<div class="staff">
+							<div class="img-wrap d-flex align-items-stretch justify-content-end">
+								<div class="img align-self-stretch" style="background-image: url(images/sponsors/KS.jpg);"></div>
+							</div>
+							<div class="text d-flex align-items-center pt-3">
+								<div>
+								    <h3 class="mb-2">Kavisha <br>Sarees</h3>	
+		            </div>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-6 col-md-6 col-lg-3 ftco-animate">
+						<div class="staff">
+							<div class="img-wrap d-flex align-items-stretch justify-content-end">
+								<div class="img align-self-stretch" style="background-image: url(images/AP.jpg);"></div>
+							</div>
+							<div class="text d-flex align-items-center pt-3">
+                            <div>
+                            <h3 class="mb-2">Angrezi<br>Patiala</h3>
+		            </div>
+							</div>
+						</div>
+					</div>
 
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
 <?php include("footer.php") ?>
 
 
