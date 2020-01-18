3ede26e595e8ee5f7f7e5b8fc8572490ce569601
Jesdin Raphael <jesdinraphael@gmail.com>
2020-01-18 14:29:38 +0530

    create page for Contrasto
    Doodling Contest

---
+++ forms/contrasto.php
@@ -0,0 +1,58 @@
+<?php include("../header.php"); ?>
+
+<div class="comment-form-wrap pt-5">
+    <div style="margin-top:35px; margin-left:20px">
+        <h1 style="color:rgb(251, 185, 1); font-weight: bolder">CONTRASTO.</h1>
+        <h4 class="mb-5" style="margin-left: 20px">InterCollege Doodling Contest</h4>
+    </div>
+    <form action="#" class="p-5 bg-light">
+        <!-- Name -->
+        <div class="form-group">
+            <label for="name">Name *</label>
+            <input type="text" class="form-control" id="name" name="name" required>
+        </div>
+
+        <!-- Number -->
+        <div class="form-group">
+            <label for="contact">Number *</label>
+            <input type="number" class="form-control" id="contact" name="contact"  required>
+        </div>
+
+        <!--  College  -->
+        <div class="form-group">
+            <label for="college">College *</label>
+            <input type="text" class="form-control" list="colleges" id="interCollege" name="college" onchange="setVisibility()"  required/>
+            <datalist id="colleges">
+                <option>St. Francis Institute of Technology</option>
+            </datalist>
+        </div>
+
+        <div id="sfitian">
+            <div class="form-group">
+                <label for="year">Year *</label>
+                <select class="form-control" name="dept" id="dept">
+                    <option value="fe">FE</option>
+                    <option value="se">SE</option>
+                    <option value="te">TE</option>
+                    <option value="be">BE</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="department">Department *</label>
+                <select class="form-control" name="dept" id="dept">
+                    <option value="comp">COMPUTER</option>
+                    <option value="it">IT</option>
+                    <option value="extc">EXTC</option>
+                    <option value="elec">Electrical</option>
+                    <option value="mech">Mechanical</option>
+                </select>
+            </div>
+        </div>
+        
+       <div class="form-group">
+            <input type="submit" value="Post Comment" class="btn py-3 px-4 btn-primary">
+        </div>
+    </form>
+</div>
+
+<?php include("../footer.php") ?>
