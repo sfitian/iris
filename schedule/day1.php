817a9c7d45aad4ada4eef6ced65d41fa3f895b01
kashyap12-byte <talatikashyap@gmail.com>
2020-01-20 10:56:16 +0530

    Add images for inauguration

--- schedule/day1.php
+++ schedule/day1.php
@@ -1,19 +1,19 @@
 <!-- --------------------- day 1 --------------------- -->
 <div class="tab-pane fade show active" id="v-pills-6" role="tabpanel" aria-labelledby="day-6-tab">
                       <div class="speaker-wrap ftco-animate d-flex">
-                          <div class="img speaker-img" style="background-image: url(images/person_1.jpg);"></div>
+                          <div class="img speaker-img" style="background-image: url(images/inaugration.jpg);"></div>
                           <div class="text pl-md-5">
                               <span class="time">10:00AM - 11:00AM</span>
                               <h2><a href="#">Committee Inauguration</a></h2>
-                              <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
+                              <p></p>
                               <!-- <h3 class="speaker-name">&mdash; <a href="#">Brett Morgan</a> <span class="position">Founder of Wordpress</span></h3> -->
                           </div>
                       </div>
                       <div class="speaker-wrap ftco-animate d-flex">
-                          <div class="img speaker-img" style="background-image: url(images/person_2.jpg);"></div>
+                          <div class="img speaker-img" style="background-image: url(images/convergence.png);"></div>
                           <div class="text pl-md-5">
                               <span class="time">11:00AM - 01:00PM</span>
-                              <h2><a href="#">Celebrity Appearance</a></h2>
+                              <h2><a href="#">Catch Up With Divya Khosla Kumar</a></h2>
                               <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
                               <!-- <h3 class="speaker-name">&mdash; <a href="#">Brett Morgan</a> <span class="position">Founder of Wordpress</span></h3> -->
                           </div>
