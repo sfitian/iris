3eb68da1a8975515a4e3773a216e5a1f13d8f024
rahul12345677 <48649507+rahul12345677@users.noreply.github.com>
2020-01-20 14:09:34 +0530

    Edit rich link

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <meta property="og:title" content="IRIS SFIT 2020"/>
+    <meta property="og:title" content="IRIS-SFIT 2020"/>
     <meta property="og:image" content="https://iris.sfit.ac.in/images/icons/themeLogo-min.png"/>
     <meta property="og:type" content="website"/>
     <meta property="og:url" content="https://iris.sfit.ac.in"/>
