d818a07ba1be07e698b09b482d67bc25d46713c0
kamathprasad9 <54414375+kamathprasad9@users.noreply.github.com>
2020-01-21 21:26:18 +0530

    Final pre iris

--- schedule/blackFri.php
+++ schedule/blackFri.php
@@ -10,7 +10,7 @@
 	              		</div>
 	              	</div>
 	              	<div class="speaker-wrap ftco-animate d-flex">
-	              		<div class="img speaker-img" style="background-image: url(images/icons/themeLogo-min2.png);"></div>
+	              		<div class="img speaker-img" style="background-image: url(images/Bhangda.jpg);"></div>
 	              		<div class="text pl-md-5">
 	              			<!-- <span class="time">11:00AM - 01:00PM</span> -->
 	              			<h2><a href="#">Bhangda</a></h2>
@@ -22,7 +22,7 @@
 	              		<div class="img speaker-img" style="background-image:  url(images/Lagori.png);"></div>
 	              		<div class="text pl-md-5">
 	              			<!-- <span class="time">01:00PM - 03:00PM</span> -->
-	              			<h2><a href="#">Lagori/Giant Jenga</a></h2>
+	              			<h2><a href="#">Game: Lagori/Giant Jenga</a></h2>
 	              			<!-- <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p> -->
 	              			<!-- <h3 class="speaker-name">&mdash; <a href="#">Brett Morgan</a> <span class="position">Founder of Wordpress</span></h3> -->
 	              		</div>
@@ -31,7 +31,7 @@
 	              		<div class="img speaker-img" style="background-image:  url(images/tugOfWar.jpeg);"></div>
 	              		<div class="text pl-md-5">
 	              			<!-- <span class="time">03:00PM - 04:30PM</span> -->
-	              			<h2><a href="#">Tug of war CMPN</a></h2>
+	              			<h2><a href="#">Game: Tug of war (CMPN)</a></h2>
 	              			<!-- <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p> -->
 	              			<!-- <h3 class="speaker-name">&mdash; <a href="#">Brett Morgan</a> <span class="position">Founder of Wordpress</span></h3> -->
 	              		</div>
@@ -40,7 +40,7 @@
 	              		<div class="img speaker-img" style="background-image:  url(images/Chole.jpg);"></div>
 	              		<div class="text pl-md-5">
 	              			<!-- <span class="time">04:30PM</span> -->
-	              			<h2><a href="#">Chole-bature/ Paratha and lassi</a></h2>
+	              			<h2><a href="#">Food: Chole-bature/ Paratha and lassi</a></h2>
 	              			<!-- <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p> -->
 	              			<!-- <h3 class="speaker-name">&mdash; <a href="#">Brett Morgan</a> <span class="position">Founder of Wordpress</span></h3> -->
 	              		</div>
