be0366164d3a22956c735119a34b7ee2e6d1c84f
ansel2000 <ansel20@gmail.com>
2020-01-21 11:06:34 +0530

    Team page

---
+++ team.php
@@ -0,0 +1,264 @@
+<?php include("header.php") ?>
+ <!--==========================
+      Sponsors Section
+    ============================-->
+
+    <section class="ftco-section ftco-speakers" id="speakers-section">
+			<div class="container">
+				<div class="row justify-content-center pb-5">
+          <div class="col-md-12 heading-section heading-section-white text-center ftco-animate">
+          	<span class="subheading">IRIS Committee</span>
+            <h2 class="mb-4">Our Workforce</h2>
+            <!-- <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p> -->
+          </div>
+        </div>
+        <style>
+            .team{
+                text-shadow: -1px 0 grey, 0 1px grey, 1px 0 grey, 0 -1px grey;
+            }
+        </style>
+				<div class="row justify-content-center pb-5">
+          <div class="col-md-12 heading-section text-center ftco-animate">
+            <h3 class="mb-4" id="whiteCol">Website Developers</h3>
+            <!-- <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p> -->
+          </div>
+        </div>
+        <div class="row">
+					<div class="col-sm-6 col-md-6 col-lg-3 ftco-animate">
+						<div class="staff">
+							<div class="img-wrap d-flex align-items-stretch justify-content-end">
+								<div class="img align-self-stretch" style="background-image: url(images/tech/Sanket.jpg);"></div>
+							</div>
+							<div class="text d-flex align-items-center pt-3">
+								<div>
+									<h3 class="mb-2 team">Sanket<br>Dalvi</h3>
+									<span class="position mb-4">Technical Secretary</span>
+									<ul class="ftco-social">
+		                <li class="ftco-animate"><a href="https://github.com/sank8dalvi"><span class="icon-github"></span></a></li>
+		                <li class="ftco-animate"><a href="https://www.linkedin.com/in/sank8dalvi/"><span class="icon-linkedin"></span></a></li>
+		                <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+		                <li class="ftco-animate"><a href="https://www.instagram.com/sank8dalvi/"><span class="icon-instagram"></span></a></li>
+		              </ul>
+		            </div>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-6 col-md-6 col-lg-3 ftco-animate">
+						<div class="staff">
+							<div class="img-wrap d-flex align-items-stretch justify-content-end">
+								<div class="img align-self-stretch" style="background-image: url(images/staff-2.jpg);"></div>
+							</div>
+							<div class="text d-flex align-items-center pt-3">
+								<div>
+									<h3 class="mb-2 team">Shivam<br>Sinha</h3>
+									<span class="position mb-4">Technical Head</span>
+									<ul class="ftco-social">
+		                <li class="ftco-animate"><a href="#"><span class="icon-github"></span></a></li>
+		                <li class="ftco-animate"><a href="#"><span class="icon-linkedin"></span></a></li>
+		                <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+		                <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+		              </ul>
+		            </div>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-6 col-md-6 col-lg-3 ftco-animate">
+						<div class="staff">
+							<div class="img-wrap d-flex align-items-stretch justify-content-end">
+								<div class="img align-self-stretch" style="background-image: url(images/tech/Dwayne.jpg);"></div>
+							</div>
+							<div class="text d-flex align-items-center pt-3">
+								<div>
+									<h3 class="mb-2 team">Dwayne <br>Fernandes</h3>
+									<span class="position mb-4">Technical Head</span>
+									<ul class="ftco-social">
+		                <li class="ftco-animate"><a href="https://github.com/dwayne99"><span class="icon-github"></span></a></li>
+		                <li class="ftco-animate"><a href="#"><span class="icon-linkedin"></span></a></li>
+		                <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+		                <li class="ftco-animate"><a href="https://www.instagram.com/dwaynefernandes/"><span class="icon-instagram"></span></a></li>
+		              </ul>
+		            </div>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-6 col-md-6 col-lg-3 ftco-animate">
+						<div class="staff">
+							<div class="img-wrap d-flex align-items-stretch justify-content-end">
+								<div class="img align-self-stretch" style="background-image: url(images/staff-4.jpg);"></div>
+							</div>
+							<div class="text d-flex align-items-center pt-3">
+								<div>
+									<h3 class="mb-2 team">Jesdin <br>Raphael</h3>
+									<span class="position mb-4">Technical Executive</span>
+					    <ul class="ftco-social">
+                            <li class="ftco-animate"><a href="https://github.com/jesdin"><span class="icon-github"></span></a></li>
+		                    <li class="ftco-animate"><a href="https://www.linkedin.com/in/jesdin-raphael-776b02194/"><span class="icon-linkedin"></span></a></li>
+		                    <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+		                    <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+		              </ul>
+		            </div>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-sm-6 col-md-6 col-lg-3 ftco-animate">
+						<div class="staff">
+							<div class="img-wrap d-flex align-items-stretch justify-content-end">
+								<div class="img align-self-stretch" style="background-image: url(images/tech/Ansel.jpg);"></div>
+							</div>
+							<div class="text d-flex align-items-center pt-3">
+								<div>
+									<h3 class="mb-2 team">Ansel <br>Dsouza</h3>
+									<span class="position mb-4">Technical Executive</span>
+                       <ul class="ftco-social">
+                            <li class="ftco-animate"><a href="#"><span class="icon-github"></span></a></li>
+		                    <li class="ftco-animate"><a href="#"><span class="icon-linkedin"></span></a></li>
+		                    <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+		                    <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+                      </ul>     
+                    </div>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-6 col-md-6 col-lg-3 ftco-animate">
+						<div class="staff">
+							<div class="img-wrap d-flex align-items-stretch justify-content-end">
+								<div class="img align-self-stretch" style="background-image: url(images/person_2.jpg);"></div>
+							</div>
+							<div class="text d-flex align-items-center pt-3">
+								<div>
+									<h3 class="mb-2 team">Alan <br>Dsilva</h3>
+									<span class="position mb-4">Technical Executive</span>
+									<ul class="ftco-social">
+                                    <li class="ftco-animate"><a href="#"><span class="icon-github"></span></a></li>
+		                    <li class="ftco-animate"><a href="#"><span class="icon-linkedin"></span></a></li>
+		                    <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+		                    <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+		              </ul>
+		            </div>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-6 col-md-6 col-lg-3 ftco-animate">
+						<div class="staff">
+							<div class="img-wrap d-flex align-items-stretch justify-content-end">
+								<div class="img align-self-stretch" style="background-image: url(images/person_3.jpg);"></div>
+							</div>
+							<div class="text d-flex align-items-center pt-3">
+								<div>
+									<h3 class="mb-2 team">Prasad <br>Kamath</h3>
+									<span class="position mb-4">Technical Executive</span>
+									<ul class="ftco-social">
+                                    <li class="ftco-animate"><a href="#"><span class="icon-github"></span></a></li>
+		                    <li class="ftco-animate"><a href="#"><span class="icon-linkedin"></span></a></li>
+		                    <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+		                    <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+		              </ul>
+		            </div>
+							</div>
+						</div>
+                    </div>
+                    <div class="col-sm-6 col-md-6 col-lg-3 ftco-animate">
+						<div class="staff">
+							<div class="img-wrap d-flex align-items-stretch justify-content-end">
+								<div class="img align-self-stretch" style="background-image: url(images/person_3.jpg);"></div>
+							</div>
+							<div class="text d-flex align-items-center pt-3">
+								<div>
+									<h3 class="mb-2 team">Ashley <br>Dsouza</h3>
+									<span class="position mb-4">Technical Executive</span>
+									<ul class="ftco-social">
+                                    <li class="ftco-animate"><a href="#"><span class="icon-github"></span></a></li>
+		                    <li class="ftco-animate"><a href="#"><span class="icon-linkedin"></span></a></li>
+		                    <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+		                    <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+		              </ul>
+		            </div>
+							</div>
+						</div>
+                    </div>
+                    <div class="col-sm-6 col-md-6 col-lg-3 ftco-animate">
+						<div class="staff">
+							<div class="img-wrap d-flex align-items-stretch justify-content-end">
+								<div class="img align-self-stretch" style="background-image: url(images/person_3.jpg);"></div>
+							</div>
+							<div class="text d-flex align-items-center pt-3">
+								<div>
+									<h3 class="mb-2 team">Gilston <br>Dsouza</h3>
+									<span class="position mb-4">Technical Executive</span>
+									<ul class="ftco-social">
+                                    <li class="ftco-animate"><a href="#"><span class="icon-github"></span></a></li>
+		                    <li class="ftco-animate"><a href="#"><span class="icon-linkedin"></span></a></li>
+		                    <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+		                    <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+		              </ul>
+		            </div>
+							</div>
+						</div>
+                    </div>
+                    <div class="col-sm-6 col-md-6 col-lg-3 ftco-animate">
+						<div class="staff">
+							<div class="img-wrap d-flex align-items-stretch justify-content-end">
+								<div class="img align-self-stretch" style="background-image: url(images/person_3.jpg);"></div>
+							</div>
+							<div class="text d-flex align-items-center pt-3">
+								<div>
+									<h3 class="mb-2 team">Kashyap <br>Talati</h3>
+									<span class="position mb-4">Technical Executive</span>
+									<ul class="ftco-social">
+                                    <li class="ftco-animate"><a href="#"><span class="icon-github"></span></a></li>
+		                    <li class="ftco-animate"><a href="#"><span class="icon-linkedin"></span></a></li>
+		                    <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+		                    <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+		              </ul>
+		            </div>
+							</div>
+						</div>
+                    </div>
+                    <div class="col-sm-6 col-md-6 col-lg-3 ftco-animate">
+						<div class="staff">
+							<div class="img-wrap d-flex align-items-stretch justify-content-end">
+								<div class="img align-self-stretch" style="background-image: url(images/person_3.jpg);"></div>
+							</div>
+							<div class="text d-flex align-items-center pt-3">
+								<div>
+									<h3 class="mb-2 team">Rohit <br>Choudhari</h3>
+									<span class="position mb-4">Technical Executive</span>
+									<ul class="ftco-social">
+                                    <li class="ftco-animate"><a href="#"><span class="icon-github"></span></a></li>
+		                    <li class="ftco-animate"><a href="#"><span class="icon-linkedin"></span></a></li>
+		                    <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+		                    <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+		              </ul>
+		            </div>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-6 col-md-6 col-lg-3 ftco-animate">
+						<div class="staff">
+							<div class="img-wrap d-flex align-items-stretch justify-content-end">
+								<div class="img align-self-stretch" style="background-image: url(images/person_4.jpg);"></div>
+							</div>
+							<div class="text d-flex align-items-center pt-3">
+								<div>
+									<h3 class="mb-2 team">Abhishek <br>Shinde</h3>
+									<span class="position mb-4">Technical Executive</span>
+									<ul class="ftco-social">
+                                    <li class="ftco-animate"><a href="#"><span class="icon-github"></span></a></li>
+		                    <li class="ftco-animate"><a href="#"><span class="icon-linkedin"></span></a></li>
+		                    <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+		                    <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+		              </ul>
+		            </div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+<?php include("footer.php") ?>
+
+
+
+
