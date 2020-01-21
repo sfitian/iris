3db52786700ce2a701312d85b57f2fbc8aefe434
kashyap12-byte <talatikashyap@gmail.com>
2020-01-21 16:20:56 +0530

    add gallery

--- Gallery.php
+++ Gallery.php
@@ -23,41 +23,37 @@
 </div>
 </div>
 <div class="row" id="lightgallery">
-<div class="col-sm-6 col-md-4 col-lg-3 col-xl-2 item aos-init aos-animate" data-aos="fade" data-src="images/big-images/nature_big_1.jpg" data-sub-html="<h4>Fading Light</h4><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor doloremque hic excepturi fugit, sunt impedit fuga tempora, ad amet aliquid?</p>">
-<a href="#"><img src="images/gallery/gall1.jpg" alt="IMage" class="img-fluid"></a>
-</div>
-<div class="col-sm-6 col-md-4 col-lg-3 col-xl-2 item aos-init aos-animate" data-aos="fade" data-src="images/big-images/nature_big_2.jpg" data-sub-html="<h4>Fading Light</h4><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam omnis quaerat molestiae, praesentium. Ipsam, reiciendis. Aut molestiae animi earum laudantium.</p>">
+
+<div class="gallery-box" data-aos="fade" data-src="images/big-images/nature_big_2.jpg" data-sub-html="<h4>Fading Light</h4><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam omnis quaerat molestiae, praesentium. Ipsam, reiciendis. Aut molestiae animi earum laudantium.</p>">
 <a href="#"><img src="images/gallery/gall2.jpg" alt="IMage" class="img-fluid"></a>
 </div>
-<div class="col-sm-6 col-md-4 col-lg-3 col-xl-2 item aos-init aos-animate" data-aos="fade" data-src="images/big-images/nature_big_3.jpg" data-sub-html="<h4>Fading Light</h4><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem reiciendis, dolorum illo temporibus culpa eaque dolore rerum quod voluptate doloribus.</p>">
+<div class="gallery-box" data-aos="fade" data-src="images/big-images/nature_big_3.jpg" data-sub-html="<h4>Fading Light</h4><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem reiciendis, dolorum illo temporibus culpa eaque dolore rerum quod voluptate doloribus.</p>">
 <a href="#"><img src="images/gallery/gall3.jpg" alt="IMage" class="img-fluid"></a>
 </div>
-<div class="col-sm-6 col-md-4 col-lg-3 col-xl-2 item aos-init aos-animate" data-aos="fade" data-src="images/big-images/nature_big_4.jpg" data-sub-html="<h4>Fading Light</h4><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim perferendis quae iusto omnis praesentium labore tempore eligendi quo corporis sapiente.</p>">
+<div class="gallery-box" data-aos="fade" data-src="images/big-images/nature_big_4.jpg" data-sub-html="<h4>Fading Light</h4><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim perferendis quae iusto omnis praesentium labore tempore eligendi quo corporis sapiente.</p>">
 <a href="#"><img src="images/gallery/gall4.jpg" alt="IMage" class="img-fluid"></a>
 </div>
-<div class="col-sm-6 col-md-4 col-lg-3 col-xl-2 item aos-init" data-aos="fade" data-src="images/big-images/nature_big_5.jpg" data-sub-html="<h4>Fading Light</h4><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, voluptatum voluptate tempore aliquam, dolorem distinctio. In quas maiores tenetur sequi.</p>">
+<div class="gallery-box" data-aos="fade" data-src="images/big-images/nature_big_5.jpg" data-sub-html="<h4>Fading Light</h4><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, voluptatum voluptate tempore aliquam, dolorem distinctio. In quas maiores tenetur sequi.</p>">
 <a href="#"><img src="images/gallery/gall5.jpg" alt="IMage" class="img-fluid"></a>
 </div>
-<div class="col-sm-6 col-md-4 col-lg-3 col-xl-2 item aos-init" data-aos="fade" data-src="images/big-images/nature_big_6.jpg" data-sub-html="<h4>Fading Light</h4><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum cum culpa blanditiis illum, voluptatum iusto quisquam mollitia debitis quaerat maiores?</p>">
+<div class="gallery-box" data-aos="fade" data-src="images/big-images/nature_big_6.jpg" data-sub-html="<h4>Fading Light</h4><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum cum culpa blanditiis illum, voluptatum iusto quisquam mollitia debitis quaerat maiores?</p>">
 <a href="#"><img src="images/gallery/gall6.jpg" alt="IMage" class="img-fluid"></a>
 </div>
-<div class="col-sm-6 col-md-4 col-lg-3 col-xl-2 item aos-init" data-aos="fade" data-src="images/big-images/nature_big_7.jpg" data-sub-html="<h4>Fading Light</h4><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores similique impedit possimus, laboriosam enim at placeat nihil voluptatibus voluptate hic!</p>">
+<div class="gallery-box" data-aos="fade" data-src="images/big-images/nature_big_7.jpg" data-sub-html="<h4>Fading Light</h4><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores similique impedit possimus, laboriosam enim at placeat nihil voluptatibus voluptate hic!</p>">
 <a href="#"><img src="images/gallery/gall7.jpg" alt="IMage" class="img-fluid"></a>
 </div>
-<div class="col-sm-6 col-md-4 col-lg-3 col-xl-2 item aos-init" data-aos="fade" data-src="images/big-images/nature_big_8.jpg" data-sub-html="<h4>Fading Light</h4><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam vitae sed cum mollitia itaque soluta laboriosam eaque sit ratione, aliquid.</p>">
+<div class="gallery-box" data-aos="fade" data-src="images/big-images/nature_big_8.jpg" data-sub-html="<h4>Fading Light</h4><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam vitae sed cum mollitia itaque soluta laboriosam eaque sit ratione, aliquid.</p>">
 <a href="#"><img src="images/gallery/gall8.jpg" alt="IMage" class="img-fluid"></a>
 </div>
-<div class="col-sm-6 col-md-4 col-lg-3 col-xl-2 item aos-init" data-aos="fade" data-src="images/big-images/nature_big_9.jpg" data-sub-html="<h4>Fading Light</h4><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem reiciendis debitis beatae facilis quos, enim quis nobis magnam architecto earum!</p>">
+<div class="gallery-box" data-aos="fade" data-src="images/big-images/nature_big_9.jpg" data-sub-html="<h4>Fading Light</h4><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem reiciendis debitis beatae facilis quos, enim quis nobis magnam architecto earum!</p>">
 <a href="#"><img src="images/gallery/gall9.jpg" alt="IMage" class="img-fluid"></a>
 </div>
-<div class="col-sm-6 col-md-4 col-lg-3 col-xl-2 item aos-init" data-aos="fade" data-src="images/big-images/nature_big_9.jpg" data-sub-html="<h4>Fading Light</h4><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque officiis magnam, facilis nam eos perspiciatis eligendi pariatur numquam debitis quos!</p>">
+<div class="gallery-box" data-aos="fade" data-src="images/big-images/nature_big_9.jpg" data-sub-html="<h4>Fading Light</h4><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque officiis magnam, facilis nam eos perspiciatis eligendi pariatur numquam debitis quos!</p>">
 <a href="#"><img src="images/gallery/gall10.jpg" alt="IMage" class="img-fluid"></a>
 </div>
-<div class="col-sm-6 col-md-4 col-lg-3 col-xl-2 item aos-init" data-aos="fade" data-src="images/big-images/nature_big_8.jpg" data-sub-html="<h4>Fading Light</h4><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis consequatur quam et, delectus, cum iste ipsa animi eligendi obcaecati nemo.</p>">
+<div class="gallery-box" data-aos="fade" data-src="images/big-images/nature_big_8.jpg" data-sub-html="<h4>Fading Light</h4><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis consequatur quam et, delectus, cum iste ipsa animi eligendi obcaecati nemo.</p>">
 <a href="#"><img src="images/gallery/gall11.jpg" alt="IMage" class="img-fluid"></a>
 </div>
-<div class="col-sm-6 col-md-4 col-lg-3 col-xl-2 item aos-init" data-aos="fade" data-src="images/big-images/nature_big_7.jpg" data-sub-html="<h4>Fading Light</h4><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci quia illo voluptatibus numquam inventore, ab asperiores molestiae distinctio atque nihil.</p>">
-<a href="#"><img src="images/gallery/gall12.jpg" alt="IMage" class="img-fluid"></a>
 </div>
 </div>
 </div>
