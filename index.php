4eff567cfafa0e81525721ccd5b231b56d4cf604
kashyap12-byte <talatikashyap@gmail.com>
2020-01-21 12:25:06 +0530

    changed the carousel

--- index.php
+++ index.php
@@ -30,6 +30,52 @@
 	    </div>
     </section>
 
+	<div class="carousel-slider-box">
+                <div id="myslideshow" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                      <li data-target="#myslideshow" data-slide-to="0" class="active"></li>
+                      <li data-target="#myslideshow" data-slide-to="1"></li>
+                      <li data-target="#myslideshow" data-slide-to="2"></li>
+                      <li data-target="#myslideshow" data-slide-to="3"></li>
+                      <li data-target="#myslideshow" data-slide-to="4"></li>
+                      <li data-target="#myslideshow" data-slide-to="5"></li>
+                      <li data-target="#myslideshow" data-slide-to="6"></li>
+                </ol>
+
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                        <img src="images/iris1.jpeg" class="d-block w-100">
+                          </div>
+                         <div class="carousel-item">
+                            <img src="images/iris2.jpeg" class="d-block w-100">
+                         </div>
+                         <div class="carousel-item">
+                            <img src="images/iris3.jpg" class="d-block w-100">
+                         </div>
+                         <div class="carousel-item">
+                            <img src="images/iris4.jpg" class="d-block w-100">
+                         </div>
+                         <div class="carousel-item">
+                            <img src="images/iris5.jpg" class="d-block w-100">
+                          
+                         </div>
+                         <div class="carousel-item">
+                            <img src="images/iris6.jpeg" class="d-block w-100">
+                         </div>
+                         <div class="carousel-item">
+                            <img src="images/iris7.jpg" class="d-block w-100">
+                         </div>
+                    </div>
+                    <a class="left carousel-control" href="#myslideshow" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myslideshow" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+                </div>
+            </div>
 
 
 
