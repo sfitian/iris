1b4b1137601da4e46f042ac9bcee348fc52c0bdc
sank8dalvi <sanket.dalvi.ssd@gmail.com>
2020-01-22 00:54:10 +0530

    Add Raagas to menu bar

--- forms/header.php
+++ forms/header.php
@@ -39,7 +39,7 @@
 </head>
 
 <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
-    <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light site-navbar-target" id="ftco-navbar">
+    <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light site-navbar-target scrolled awake" id="ftco-navbar">
         <div class="container">
             <a class="navbar-brand" href="../index.php"> <img src="../images/icons/irisLogo.png" class="irisLogo"></a>
             <button class="navbar-toggler js-fh5co-nav-toggle fh5co-nav-toggle" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
@@ -54,7 +54,8 @@
                 <li class="nav-item"><a href="#gallery-section" class="nav-link"><span>Gallery</span></a></li>
 -->            
                 <li class="nav-item"><a href="../schedule.php" class="nav-link"><span>Schedule</span></a></li>                
-                <li class="nav-item"><a href="../competitions.php" class="nav-link"><span>Competitions</span></a></li>
+                <li class="nav-item"><a href="../competitions.php" class="nav-link"><span>Competitions</span></a></li>         
+                <li class="nav-item"><a href="raagas/" class="nav-link"><span>Raagas</span></a></li>
                 <li class="nav-item"><a href="../sponsor.php" class="nav-link"><span>Sponsors</span></a></li>
                 <li class="nav-item cta"><a href="../competitions.php" class="nav-link">Register</a></li>
             </ul>
