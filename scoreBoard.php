4841e3dc37242003f441835aaac98c7388917228
Jesdin Raphael <jesdinraphael@gmail.com>
2020-01-24 08:04:44 +0530

    add score Board for FE

---
+++ scoreBoard.php
@@ -0,0 +1,149 @@
+<?php include("header.php") ?>
+
+<section class="ftco-section" id="pricing-section">
+   <div class="container">
+       <!-- FE -->
+      <!-- Heading -->
+      <h1 style="text-align:center">FE</h1>
+      <p style="text-align:center">BOYS</p>
+      <div class="row">
+         <div class="col-md-3 ftco-animate">
+            <div class="pricing-entry pb-1 text-center">
+               <div class="row">
+                  <div class="col-md">
+                     <p><span class="price">FE CMPN A</span></p>
+                     <h2>0</h2>
+                  </div>
+                  <div class="col-md">
+                  <p><span class="price">FE CMPN B</span></p>
+                  <h2>0</h2>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="col-md-3 ftco-animate">
+            <div class="pricing-entry pb-1 text-center">
+               <div class="row">
+                  <div class="col-md">
+                     <p><span class="price">FE INFT A</span></p>
+                     <h2>0</h2>
+                  </div>
+                  <div class="col-md">
+                    <p><span class="price">FE INFT B</span></p>
+                    <h2>0</h2>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="col-md-3 ftco-animate">
+            <div class="pricing-entry pb-1 text-center">
+               <div class="row">
+                  <div class="col-md">
+                     <p><span class="price">FE EXTC A</span></p>
+                     <h2>0</h2>
+                  </div>
+                  <div class="col-md">
+                  <p><span class="price">FE EXTC B</span></p>
+                  <h2>0</h2>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="col-md-3 ftco-animate">
+            <div class="pricing-entry pb-1 text-center">
+               <div class="row">
+                  <div class="col-md">
+                     <p><span class="price">FE ELEC A</span></p>
+                     <h2>0</h2>
+                  </div>
+                  <div class="col-md">
+                  <p><span class="price">FE ELEC B</span></p>
+                  <h2>0</h2>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      
+      <p style="text-align:center">GIRLS</p>
+      <div class="row">
+         <div class="col-md-3 ftco-animate">
+            <div class="pricing-entry pb-1 text-center">
+               <div class="row">
+                  <div class="col-md">
+                     <p><span class="price">FE CMPN A</span></p>
+                     <h2>0</h2>
+                  </div>
+                  <div class="col-md">
+                  <p><span class="price">FE CMPN B</span></p>
+                  <h2>0</h2>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="col-md-3 ftco-animate">
+            <div class="pricing-entry pb-1 text-center">
+               <div class="row">
+                  <div class="col-md">
+                     <p><span class="price">FE INFT A</span></p>
+                     <h2>0</h2>
+                  </div>
+                  <div class="col-md">
+                    <p><span class="price">FE INFT B</span></p>
+                    <h2>0</h2>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="col-md-3 ftco-animate">
+            <div class="pricing-entry pb-1 text-center">
+               <div class="row">
+                  <div class="col-md">
+                     <p><span class="price">FE EXTC A</span></p>
+                     <h2>0</h2>
+                  </div>
+                  <div class="col-md">
+                  <p><span class="price">FE EXTC B</span></p>
+                  <h2>0</h2>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="col-md-3 ftco-animate">
+            <div class="pricing-entry pb-1 text-center">
+               <div class="row">
+                  <div class="col-md">
+                     <p><span class="price">FE ELEC A</span></p>
+                     <h2>0</h2>
+                  </div>
+                  <div class="col-md">
+                  <p><span class="price">FE ELEC B</span></p>
+                  <h2>0</h2>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+    </div>
+</section>
+
+<style>
+.pricing-entry p .price {
+    font-size: 1em;
+}
+
+.ftco-navbar-light .navbar-nav > .nav-item > .nav-link {
+    font-size: 16px;
+    padding-top: .7rem;
+    padding-bottom: .7rem;
+    padding-left: 20px;
+    padding-right: 20px;
+    color: #000000;
+    font-weight: 400;
+    opacity: 1 !important; }
+    @media (max-width: 991.98px) {
+      .ftco-navbar-light .navbar-nav > .nav-item > .nav-link {
+        color: #fff; } }
+</style>
+
+<?php include("footer.php") ?>
