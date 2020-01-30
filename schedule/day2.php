d25e51962ebff32d967729bde8e700fcde5e6c6b
kamathprasad9 <54414375+kamathprasad9@users.noreply.github.com>
2020-01-29 20:20:53 -0800

    Update Schedule day2

--- schedule/day2.php
+++ schedule/day2.php
@@ -1,10 +1,19 @@
 <!-- --------------------- day 2 --------------------- -->
 <div class="tab-pane fade" id="v-pills-7" role="tabpanel" aria-labelledby="v-pills-day-7-tab">
                       <div id="test" class="speaker-wrap ftco-animate d-flex">
-                          <div class="img speaker-img" style="background-image: url(images/band.jpg);"></div>
+                          <div class="img speaker-img" style="background-image: url(images/convergence1.png);"></div>
                           <div class="text pl-md-5">
-                              <span class="time">10:00AM - 11:30AM</span>
-                              <h2><a href="#">Band (opening)</a></h2>
+                              <span class="time">10:00AM - 11:00AM</span>
+                              <h2><a href="#">Teachers Games</a></h2>
+                              <p></p>
+                              <!-- <h3 class="speaker-name">&mdash; <a href="#">Brett Morgan</a> <span class="position">Founder of Wordpress</span></h3> -->
+                          </div>
+                      </div>
+                      <div class="speaker-wrap ftco-animate d-flex">
+                          <div class="img speaker-img" style="background-image: url(images/tugOfWar.jpeg);"></div>
+                          <div class="text pl-md-5">
+                              <span class="time">11:00AM - 12:00PM</span>
+                              <h2><a href="#">Tug of War - Finals</a></h2>
                               <p></p>
                               <!-- <h3 class="speaker-name">&mdash; <a href="#">Brett Morgan</a> <span class="position">Founder of Wordpress</span></h3> -->
                           </div>
@@ -12,7 +21,7 @@
                       <div class="speaker-wrap ftco-animate d-flex">
                           <div class="img speaker-img" style="background-image: url(images/convergence1.png);"></div>
                           <div class="text pl-md-5">
-                              <span class="time">11:00AM - 01:00PM</span>
+                              <span class="time">12:00AM - 01:00PM</span>
                               <h2><a href="#">Celebrity Act</a></h2>
                               <p></p>
                               <!-- <h3 class="speaker-name">&mdash; <a href="#">Brett Morgan</a> <span class="position">Founder of Wordpress</span></h3> -->
@@ -21,17 +30,17 @@
                       <div class="speaker-wrap ftco-animate d-flex" onclick="location.href='forms/insync.php';">
                           <div class="img speaker-img" style="background-image: url(images/dance.jpg);"></div>
                           <div class="text pl-md-5">
-                              <span class="time">01:00PM- 03:00PM</span>
-                              <h2><a href="forms/insync.php">Intercollege Dance</a></h2>
+                              <span class="time">01:00PM- 02:30PM</span>
+                              <h2><a href="forms/insync.php">INSYNC</a></h2>
                               <span class="time"> <button type="button" class="btn btn-primary">Register Now</button></span>
-                              <p></p>
+                              <p>Intercollege Dance Competition</p>
                               <!-- <h3 class="speaker-name">&mdash; <a href="#">Brett Morgan</a> <span class="position">Founder of Wordpress</span></h3> -->
                           </div>
                       </div>
                       <div class="speaker-wrap ftco-animate d-flex" onclick="location.href='forms/iris.php';">
                           <div class="img speaker-img" style="background-image: url(images/fashion.png);"></div>
                           <div class="text pl-md-5">
-                              <span class="time">03:00PM - 04:00PM</span>
+                              <span class="time">02:30PM - 03:30PM</span>
                               <h2><a href="#">Mr & Ms Iris (Fashion show)</a></h2>
                               <span class="time"> <button type="button" class="btn btn-primary">Register Now</button></span>
                                
